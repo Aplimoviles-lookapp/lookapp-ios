c47a8b549aa606d8d4a84ac044793a386ee5e279
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2025-05-30 22:37:25 -0500

    first commit

---
+++ lookapp/lookapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lookapp
+//
+//  Created by jojan on 30/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("LookApp!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lookapp/lookapp/lookappApp.swift
@@ -0,0 +1,17 @@
+//
+//  lookappApp.swift
+//  lookapp
+//
+//  Created by jojan on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct lookappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
