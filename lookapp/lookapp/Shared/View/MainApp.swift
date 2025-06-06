6355c81b2304a9179699128eed5bff4a8273b067
Jojan Esteban Serna <78891995+Jojan-Esteban-Serna@users.noreply.github.com>
2025-06-05 21:45:24 -0500

    Update MainApp.swift

--- lookapp/lookapp/Shared/View/MainApp.swift
+++ lookapp/lookapp/Shared/View/MainApp.swift
@@ -3,12 +3,12 @@ import SwiftUI
 struct MainApp: View {
     @State private var selectedTab: Tab = .home
 
-    enum Tab: String, CaseIterable {
+    enum Tab: String {
         case home = "Principal"
         case search = "Buscar"
         case saved = "Guardados"
         case notifications = "Notificaciones"
-        
+
         var icon: String {
             switch self {
             case .home: return "house"
@@ -22,26 +22,42 @@ struct MainApp: View {
     var body: some View {
         NavigationView {
             TabView(selection: $selectedTab) {
-                ForEach(Tab.allCases, id: \.self) { tab in
-                    DummyScreen(title: tab.rawValue)
-                        .tabItem {
-                            Label(tab.rawValue, systemImage: tab.icon)
-                        }
-                        .tag(tab)
-                }
+                HomeView()
+                    .tabItem {
+                        Label(Tab.home.rawValue, systemImage: Tab.home.icon)
+                    }
+                    .tag(Tab.home)
+
+                SearchView()
+                    .tabItem {
+                        Label(Tab.search.rawValue, systemImage: Tab.search.icon)
+                    }
+                    .tag(Tab.search)
+
+                SavedView()
+                    .tabItem {
+                        Label(Tab.saved.rawValue, systemImage: Tab.saved.icon)
+                    }
+                    .tag(Tab.saved)
+
+                NotificationsView()
+                    .tabItem {
+                        Label(Tab.notifications.rawValue, systemImage: Tab.notifications.icon)
+                    }
+                    .tag(Tab.notifications)
             }
             .navigationBarTitle(Text(selectedTab.rawValue), displayMode: .inline)
             .navigationBarItems(
                 leading:
-                    NavigationLink(destination: DummyScreen(title: "Creditos")) {
-                        Image(systemName: "info").foregroundColor(.blue)
-                    }
-                    .padding(),
+                    NavigationLink(destination: CreditosView()) {
+                        Image(systemName: "info")
+                            .foregroundColor(.blue)
+                    },
                 trailing:
-                    NavigationLink(destination: DummyScreen(title: "Profile")) {
-                        Image(systemName: "person").foregroundColor(.blue)
+                    NavigationLink(destination: ProfileView()) {
+                        Image(systemName: "person")
+                            .foregroundColor(.blue)
                     }
-                    .padding()
             )
         }
     }
